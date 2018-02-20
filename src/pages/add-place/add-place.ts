fe1004f7715c98ab727c4d3fa09a910ffc7a3772
Chris <chris_skart@hotmail.com>
2018-02-20 23:48:10 +0200

    Alerts implementation

--- src/pages/add-place/add-place.ts
+++ src/pages/add-place/add-place.ts
@@ -9,6 +9,8 @@ import { SetLocationPage } from "../set-location/set-location";
 import { Location } from "../../models/location";
 import { PlacesService } from "../../services/places";
 import {normalizeURL} from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+
 
 declare var cordova: any;
 
@@ -30,10 +32,23 @@ export class AddPlacePage {
               private placesService: PlacesService,
               private geolocation: Geolocation,
               private camera: Camera,
-              private file: File) {
+              private file: File,
+              private alertCtrl: AlertController) {
   }
 
-  onSubmit(form: NgForm) {
+
+
+    presentAlert() {
+        let alert = this.alertCtrl.create({
+            title: 'Your item was added',
+            subTitle: 'Memory just created',
+            buttons: ['OK']
+        });
+        alert.present();
+    }
+
+
+    onSubmit(form: NgForm) {
     this.placesService
       .addPlace(form.value.title, form.value.description, this.location, this.imageUrl);
     form.reset();
--- src/pages/place/place.ts
+++ src/pages/place/place.ts
@@ -3,6 +3,8 @@ import { NavParams, ViewController } from 'ionic-angular';
 
 import { Place } from "../../models/place";
 import { PlacesService } from "../../services/places";
+import { AlertController } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-place',
@@ -13,11 +15,37 @@ export class PlacePage {
   index: number;
 
   constructor(public navParams: NavParams,
+              private alertCtrl: AlertController,
               private viewCtrl: ViewController,
               private placesService: PlacesService) {
     this.place = this.navParams.get('place');
     this.index = this.navParams.get('index');
   }
+
+    presentConfirm() {
+        let alert = this.alertCtrl.create({
+            title: 'Delete this Item',
+            message: 'Do you want to delete this memory?',
+            buttons: [
+                {
+                    text: 'Cancel',
+                    role: 'cancel',
+                    handler: () => {
+                        console.log('Cancel clicked');
+                    }
+                },
+                {
+                    text: 'Delete',
+                    handler: () => {
+                        this.onDelete();
+                        console.log('Item Deleted');
+                    }
+                }
+            ]
+        });
+        alert.present();
+    }
+
 
   onLeave() {
     this.viewCtrl.dismiss();
@@ -27,4 +55,5 @@ export class PlacePage {
     this.placesService.deletePlace(this.index);
     this.onLeave();
   }
+
 }
